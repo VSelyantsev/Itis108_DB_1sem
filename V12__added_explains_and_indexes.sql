b58eab2a78d7916f60b6d8f855086c2244c3c1e0
VSelyantsev <vselyantsev@mail.ru>
2022-11-17 14:33:48 +0300

    added homework about explain analyse and indexes

---
+++ V12__added_explains_and_indexes.sql
@@ -0,0 +1,48 @@
+-------------------------------------------------------
+-- EXPLAIN ANALYSE --
+
+EXPLAIN ANALYSE select number, count(*) as "numbers"
+from block
+where number = 103
+group by number;
+
+EXPLAIN ANALYSE select address.hash_adr, Max(block.number) as "numbers"
+from block
+    join address on block.id = address.id
+group by address.hash_adr;
+
+EXPLAIN ANALYSE  select transactions.hash_tr as "hash",
+            Min(block.number) as "numbers"
+from block
+    join transactions on transactions.id = block.id
+group by transactions.hash_tr;
+
+EXPLAIN ANALYSE select blockchain.id_address_main_coin as "id_address",
+        Min(block.number) as "numbers"
+from block
+    join blockchain on blockchain.id = block.id
+group by blockchain.id_address_main_coin
+having Min(block.number) >= 101;
+
+EXPLAIN ANALYSE select hash_tr as "hash",
+SUM (value_tr)
+from transactions
+group by hash_tr;
+
+EXPLAIN ANALYSE select time as "block_time", size as "block_size",
+AVG(number)
+from block
+group by time, size;
+--------------------------------------------------------------------
+-- indexes --
+
+create index name_idx on blockchain(name);
+select * from blockchain where name like 'KAZANMALL';
+
+create index hash_value_tr on transactions USING HASH (value_tr);
+select * from transactions where value_tr >= 120;
+--------------------------------------------------------------------
+-- explain analyse with indexes --
+EXPLAIN ANALYSE select * from blockchain where name like 'KAZANMALL';
+
+EXPLAIN ANALYSE select * from transactions where value_tr > 110;
